3219ae9b1b0352b20af0c6b6c126f35e81a9751b
Iago Marinheiro Elias <go.marinheiro@gmail.com>
2018-11-03 16:52:17 -0300

    refactoring: adicionando commonModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {PetListItemComponent} from './pet-list-item/pet-list-item.component';
 import {PetListItemOptions} from '../pet-list-item.options';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule ( {
+  imports: [
+    CommonModule
+  ],
   declarations: [
     PetListItemComponent,
     FooterComponent,
